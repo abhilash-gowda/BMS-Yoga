2059a462649dbe04f0aeacbf8adff43b725b55fc
A Madhav Bharadwaj <madhavbharadwaj1995@gmail.com>
2018-07-06 00:12:20 +0530

    appbar to dashbaord

--- app/src/main/java/com/example/madhav/bms_yoga/HomePage/dashboard.java
+++ app/src/main/java/com/example/madhav/bms_yoga/HomePage/dashboard.java
@@ -1,23 +1,59 @@
 package com.example.madhav.bms_yoga.HomePage;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.madhav.bms_yoga.R;
 
 
 public class dashboard extends Fragment {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);//Make sure you have this line of code.
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_dashboard,container,false);
+
+        android.support.v7.widget.Toolbar myToolbar = (android.support.v7.widget.Toolbar) v.findViewById(R.id.my_toolbar);
+
+       // setSupportActionBar(myToolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(myToolbar);
+
+
+        someMethodThatUsesActivity(getActivity());
+        return v;
     }
+    void someMethodThatUsesActivity(Activity myActivityReference) {
+        myActivityReference.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
 
 }
